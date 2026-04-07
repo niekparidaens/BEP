0c6d6b2901aafcdbdf5d5c7745b44082a8758569
niekparidaens <n.t.paridaens@gmail.com>
2026-04-07 21:52:03 +0200

    changed latent_dim

--- VAE-cluster-NB-lossfunction.py
+++ VAE-cluster-NB-lossfunction.py
@@ -608,7 +608,7 @@ print(f"Validation examples: {len(val_dataset)} | Test examples: {len(test_datas
 # Hyperparameters
 epochs = 40
 beta = 1e-3
-latent_dim = 16
+latent_dim = 32
 hidden_dim = 256
 learning_rate = 5e-4
 early_stop_patience = 41
